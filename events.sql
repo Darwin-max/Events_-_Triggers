229924936e0b6b4ea24cfda1fac614cd45db716f
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-07-04 06:54:28 -0500

    feat: :sparkles: Events: Ejercicio 1

--- events.sql
+++ events.sql
@@ -1,1 +1,22 @@
+-- 1. Resumen Diario Único : crear un evento que genere un resumen de ventas 
+-- **una sola vez** al finalizar el día de ayer y 
+-- luego se elimine automáticamente llamado `ev_resumen_diario_unico`.
+DELIMITER //
 
+CREATE EVENT IF NOT EXISTS ev_resumen_diario_unico
+ON SCHEDULE AT TIMESTAMP(CURRENT_DATE, '00:05:00') + INTERVAL 1 DAY
+ON COMPLETION NOT PRESERVE
+DO
+BEGIN
+    INSERT INTO resumen_ventas (fecha, total_pedidos, total_ingresos)
+    SELECT
+        DATE(fecha_recogida) AS fecha,
+        COUNT(*) AS total_pedidos,
+        SUM(total) AS total_ingresos
+    FROM pedido
+    WHERE DATE(fecha_recogida) = CURRENT_DATE - INTERVAL 1 DAY
+    GROUP BY DATE(fecha_recogida);
+END;
+//
+
+DELIMITER ;
